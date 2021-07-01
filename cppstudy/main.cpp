9b4ee581c711d3fd94633a056a67d8af049f314e
li.rongping <li.rongping@immomo.com>
2021-07-01 10:43:26 +0800

    Initial Commit

---
+++ cppstudy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppstudy
+//
+//  Created by MOMO on 2021/7/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
